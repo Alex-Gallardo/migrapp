cdfbfb723c8df6b07136d590fa2d4ba2953f0d5e
Alex Gallardo <gallardoalex069@gmail.com>
2020-04-22 18:25:44 -0600

    Updates

---
+++ precache-manifest.f953f23e1664df8ad5bdbec08172fcd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85da5398ca1844adb88d81326cba8af7",
+    "url": "/migrapp/index.html"
+  },
+  {
+    "revision": "542dd44bd09117c615bb",
+    "url": "/migrapp/static/css/main.1c26b709.chunk.css"
+  },
+  {
+    "revision": "5d15055f92a667a5294c",
+    "url": "/migrapp/static/js/2.cfd8a547.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/migrapp/static/js/2.cfd8a547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542dd44bd09117c615bb",
+    "url": "/migrapp/static/js/main.86bb9418.chunk.js"
+  },
+  {
+    "revision": "ba0d8096cdc9d5ad73b6",
+    "url": "/migrapp/static/js/runtime-main.04681581.js"
+  },
+  {
+    "revision": "8d9877331a9171ad0edf004c987325bf",
+    "url": "/migrapp/static/media/2480748.8d987733.jpg"
+  },
+  {
+    "revision": "8547ceae0d543a17c0ca6f55ff13e606",
+    "url": "/migrapp/static/media/chico.8547ceae.svg"
+  },
+  {
+    "revision": "f3883d0dbd751749e5f9b72aa0c91502",
+    "url": "/migrapp/static/media/menu.f3883d0d.svg"
+  }
+]);
